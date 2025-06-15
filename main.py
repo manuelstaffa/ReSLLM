ca656f17827321cd48d14e39a923d9ba4f4dd647
manuelstaffa <manuel.staffa@gmail.com>
2025-06-15 11:27:29 +0200

    removed duplicate folder

--- main.py
+++ main.py
@@ -43,22 +43,6 @@ def main():
         prompter = RewardPrompter(game=game)
         prompter.master_prompt()
 
-    """print(f"Final Configuration: {config}")
-    print(
-        f"Using model: {config.get('openai.model')} with temperature: {config.get('openai.temperature')}"
-    )
-
-    print(f"Env: {config.get('env')}")
-    print(f"Env: {config['env']}")
-
-    context = {
-        "game": config.get("env.game"),
-        "model": config["openai"]["model"],
-        "temperature": config["openai.temperature"],
-    }
-    prompt = config.format("prompt.template", context=context)
-    print(f"Prompt with context: {prompt}")"""
-
 
 if __name__ == "__main__":
     main()
